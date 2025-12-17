b4990f63d955589b88c5af6743fc3a66a84f12b3
chihiroyasu <ychihiro0315@icloud.com>
2025-12-17 11:31:11 +0900

    router追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-// import BoardComponent from "./Board";
-// import DynamicBoard from "./DynamicBoard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PosterBoardComponent from "./PosterBoardComponent";
+import PosterBoardComponent1 from "./PosterBoardComponent1";
+import PosterBoardComponent2 from "./PosterBoardComponent2";
 
 function App() {
   return (
-    <div>
-      {/* <h1>Hello, React!</h1>
-      <p>Reactの勉強を始めましょう！</p> */}
-      <PosterBoardComponent />
-      {/* <BoardComponent /> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* ルート */}
+        <Route path="/" element={<PosterBoardComponent />} />
+
+        {/* 追加ルート */}
+        <Route path="/first" element={<PosterBoardComponent1 />} />
+        <Route path="/second" element={<PosterBoardComponent2 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
